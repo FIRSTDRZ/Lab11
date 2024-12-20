26278e1a1675c32454ffae04870af453e060cb2a
DUNRUZEED <159751313+FIRSTDRZ@users.noreply.github.com>
2024-12-20 22:29:05 +0700

    lab11_1

--- lab11_1.cpp
+++ lab11_1.cpp
@@ -1,2 +1,19 @@
-// cout << "Press Enter 3 times to reveal your future.";
-// cout << "You will get A in this 261102.";
+#include<iostream>
+#include<string>
+#include<ctime>
+#include<cstdlib>
+using namespace std;
+
+int main(){
+    cout<<"Press Enter 3 times to reveal your future.";
+    cin.get();
+    cin.get();
+    cin.get();
+    srand(time(0));
+    string grades[] = {"A", "B+", "B", "C+", "C", "D+", "D", "F", "W"};
+    string grade = grades[rand() % 9];
+    cout << "You will get " << grade << " in this 261102.";
+
+    return 0 ;
+}
+
